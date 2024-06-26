44b18ff86a0fb11378c90792592348f5fb7b7e0f
han-ol <g@hans.olischlaeger.com>
2024-06-26 17:22:33 +0200

    Move package files according to src layout

---
+++ src/inverse_kinematics_sbi/plot.py
@@ -0,0 +1,11 @@
+# Plotting functions for inverse kinematics problem solutionsfrom
+import matplotlib.pyplot as plt
+
+
+def plot_arms(coords, fig_name):
+    plt.figure()
+    n_arms = coords.shape[0]
+    for i in range(min(n_arms, 1000)):
+        plt.plot(coords[i, :, 0], coords[i, :, 1], ls="-")
+    plt.gca().set_aspect("equal")
+    plt.savefig(f"{fig_name}.pdf")
--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,6 @@ import pytest
 
 @pytest.fixture
 def benchmark_robot():
-    from inverse_kinematics.benchmark_robot import BenchmarkRobot
+    from inverse_kinematics_sbi.benchmark_robot import BenchmarkRobot
 
     return BenchmarkRobot()
